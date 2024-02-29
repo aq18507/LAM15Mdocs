e986fc240b5f9cadfb87299da2d9978e044e60ec
RMH Aerospace <91566934+aq18507@users.noreply.github.com>
2024-02-29 08:41:05 +0000

    autocommit: v01.02-2024-02-29@ 8:41:01

---
+++ doc/Depreciated_Scripts_GATOR.rst
@@ -0,0 +1,3 @@
+Depreciated Scripts
+===================
+
--- doc/conf.py
+++ doc/conf.py
@@ -96,9 +96,9 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
-html_logo = '../_static/GATOR_logo_v1.png'
-html_favicon = '../_static/GATOR_logo_v2.png'
+# html_theme = 'sphinx_rtd_theme'
+# html_logo = '../_static/GATOR_logo_v1.png'
+# html_favicon = '../_static/GATOR_logo_v2.png'
 html_extra_path = ['../extra/']
 
 # Theme options are theme-specific and customize the look and feel of a theme
